17b741ea65df97dac65ff0185490a2975e1879e4
rdiego821 <rdiego82@gmail.com>
2026-08-31 09:48:40 -0500

    Add fileURLToPath in astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+		},
+	},
 	integrations: [
 		starlight({
 			title: 'Senior Engineering Knowledge Hub',
